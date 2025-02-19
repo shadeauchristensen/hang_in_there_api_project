cdc97160e08722ac3a6489a889e64783ec2c37f1
Jaren Garman <jarengarman@protonmail.com>
2025-02-19 17:22:15 -0500

    test: Add initial tests for create poster

--- spec/api/v1/posters_controller_spec.rb
+++ spec/api/v1/posters_controller_spec.rb
@@ -123,5 +123,28 @@ RSpec.describe "Posters API", type: :request do
       expect(attributes).to have_key(:img_url)
       expect(attributes[:img_url]).to eq("./assets/failure.jpg")
     end
+
+    it "can create a new poster" do
+      poster_params = {
+        name: "FAILURE",
+        description: "Why bother trying? It's probably not worth it.",
+        price: 68.00,
+        year: 2019,
+        vintage: true,
+        img_url: "./assets/failure.jpg"
+      }
+      headers = {"CONTENT_TYPE" => "application/json"} # pass params as JSON rather than plain text
+
+      post "/api/v1/posters", headers: headers, params: JSON.generate(poster: poster_params)
+      created_poster = Poster.last
+
+      expect(response).to be_successful
+      expect(created_poster.name).to eq(poster_params[:name])
+      expect(created_poster.description).to eq(poster_params[:description])
+      expect(created_poster.price).to eq(poster_params[:price])
+      expect(created_poster.year).to eq(poster_params[:year])
+      expect(created_poster.vintage).to eq(poster_params[:vintage])
+      expect(created_poster.img_url).to eq(poster_params[:img_url])
+    end
   end
 end
