ca5e8b4f90e518e68e3660013123309d220e37a1
Jaren Garman <jarengarman@protonmail.com>
2025-02-20 12:59:59 -0500

    refactor: Replace manual json serializer with JSONAPI serializer

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,29 +1,29 @@
 class Api::V1::PostersController < ApplicationController
   def index
     posters = Poster.all
-    render json: PosterSerializer.format_posters(posters)
+    render json: PosterSerializer.new(posters)
   end
 
   def show
     poster = Poster.find(params[:id])
-    render json: PosterSerializer.format_poster(poster)
+    render json: PosterSerializer.new(poster)
   end
 
   def create
     poster = Poster.create(poster_params)
-    render json: PosterSerializer.format_poster(poster)
+    render json: PosterSerializer.new(poster)
   end
 
   def update
     poster = Poster.find(params[:id])
     poster.update(poster_params)
-    render json: PosterSerializer.format_poster(poster)
+    render json: PosterSerializer.new(poster)
   end
 
   def destroy
     poster = Poster.find(params[:id])
     poster.destroy
-    render json: PosterSerializer.format_poster(poster)
+    render json: PosterSerializer.new(poster)
   end
 
   private
