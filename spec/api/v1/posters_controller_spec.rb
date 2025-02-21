df41993fc046743f97fc5330e93ed7e72e7c1424
Jaren Garman <jarengarman@protonmail.com>
2025-02-21 17:54:41 -0500

    refactor: Remove do/end block around get request

--- spec/api/v1/posters_controller_spec.rb
+++ spec/api/v1/posters_controller_spec.rb
@@ -189,13 +189,12 @@ RSpec.describe "Posters API", type: :request do
     end
 
     it "filters posters by price less than 80 dollars" do
-      get "/api/v1/posters?max_price=88.00" do
-        response_data = parsed_response
+      get "/api/v1/posters?max_price=88.00"
+      response_data = parsed_response
 
-        expect(response).to be_successful
-        expect(response_data[:data].size).to eq(1)
-        expect(response_data[:data].first[:attributes][:name]).to eq("FAILURE")
-      end
+      expect(response).to be_successful
+      expect(response_data[:data].size).to eq(1)
+      expect(response_data[:data].first[:attributes][:name]).to eq("FAILURE")
     end
   end
 end
